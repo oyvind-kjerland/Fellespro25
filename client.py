a85352f99210768e52c4de0802dceb39b766e9c5
Simen S <simen@outlook.com>
2014-02-25 13:17:21 +0100

    La til server.py og client.py

---
+++ client.py
@@ -0,0 +1,95 @@
+# -*- coding: utf-8 -*-
+import socket
+import json
+
+class Client(object):
+
+    def __init__(self):
+        # Initialiser en tilkobling
+        self.connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        
+        
+        
+        
+    def printBacklog(self,backlog):
+        for linje in backlog:
+                print(linje)
+    
+    def start(self, host, port):
+        # Start tilkoblingen
+        self.connection.connect((host, port))
+
+        # Be brukeren om å skrive inn brukernavn
+        
+        canLogIn = False
+        
+        while not canLogIn:
+        
+                nick = raw_input('Enter your username: ')
+        
+                data = {'request': 'login', 'username': nick}
+                data = json.dumps(data)
+                self.send(data)
+        
+                received_data = self.connection.recv(1024)
+                data = json.loads(received_data)
+        
+                if(data.get('error')):
+                        error = data['error']
+                        if(error == 'Name allready taken!'):
+                                print(error)
+                                
+                        elif(error == 'Invalid username'):
+                                print(error)
+                        print('Try another')
+                else:
+                        print('Logged in as ' + data['username'])
+                        canLogIn = True
+                        backlog = data['messages']
+                        self.printBacklog(backlog)
+
+        message = ''
+        # Så lenge brukeren ikke skriver exit i meldingsfeltet
+        # vil programmet spørre etter tekst
+        while True:
+            message = raw_input('Enter a message (type "exit" to quit): ')
+
+            # Lukk tilkoblingen hvis brukeren skriver "exit"
+            if message == 'exit':
+                self.send(json.dumps({'nick': nick, 'message': 'I\'m leaving. Goodbye!'}))
+                self.connection.close()
+                break
+
+            # Konstruer et JSON objekt som som skal
+            # sendes til serveren
+            data = {'nick': nick, 'message': message}
+
+            # Lag en streng av JSON-objektet
+            data = json.dumps(data)
+
+            # Send meldingen til serveren
+            self.send(data)
+
+            # Motta data fra serveren
+            # Setter maks datastørrelse til 1kb
+            received_data = self.connection.recv(1024)
+
+            # Si ifra at klienten har mottatt en melding
+            print 'Received from server: ' + received_data
+
+    # Lag en metode for å sende en melding til serveren
+    def send(self, data):
+        self.connection.send(data)
+
+
+# Kjøres når programmet startes
+if __name__ == "__main__":
+    # Definer host og port for serveren
+    HOST = 'localhost'
+    PORT = 9999
+
+    # Initialiser klienten
+    client = Client()
+
+    # Start klienten
+    client.start(HOST, PORT)
---
+++ server.py
@@ -0,0 +1,88 @@
+# -*- coding: utf-8 -*-
+import SocketServer
+import json
+from datetime import datetime
+
+backlog = ['Hei','heisann']
+users = ['admin']
+
+class CLientHandler(SocketServer.BaseRequestHandler):
+
+    def send(self, data):
+        self.request.send(data)
+
+    def handle(self):
+        # Hent IP-adressen til klienten
+        self.ip = self.client_address[0]
+        
+
+        # Hent portnummeret til klienten
+        self.port = self.client_address[1]
+
+        # Si ifra at en ny klient har koblet til serveren
+        print 'Client connected @' + self.ip + ':' + str(self.port)
+        
+        canLogIn = False
+        
+        while not canLogIn:
+        
+                data = self.request.recv(1024)
+                data = json.loads(data)
+                global backlog
+        
+                if(data['request'] == 'login'):
+                        username = data['username']
+                        if username not in users:
+                                users.append(username)
+                                data = {'response': 'login', 'username': username , 'messages' : backlog}
+                                data = json.dumps(data)
+                                self.send(data)
+                                print(self.ip + ':' + str(self.port) + " Logged in as: " + username)
+                                canLogIn = True
+                        else:
+                                data = {'response': 'login','error':'Name allready taken!', 'username': username}
+                                data = json.dumps(data)
+                                self.send(data)
+                                print(self.ip + ':' + str(self.port) + " Tried to log in as " + username + ". Username taken")
+                
+                
+                if(data['request'] == 'message'):
+                        
+        #self.request.sendall(backlog)
+
+        while True:
+            # Motta data fra klienten
+            # Setter maks datastørrelse til 1kb
+            data = self.request.recv(1024)
+
+            # Avslutt hvis serveren ikke mottar data fra klienten
+            if not data: break
+
+            # Last inn JSON-objektet
+            data = json.loads(data)
+
+            # Si ifra at klienten har sendt en melding
+            print datetime.now().strftime("%Y-%m-%d %H:%M") + ' ' + data['nick'] + ': ' + data['message']
+            
+            backlog += data['message'] + ", "
+
+            # Send en melding til klienten om at meldingen ble mottatt
+            self.request.sendall('Message received')
+
+
+class ThreadedTCPServer(SocketServer.ThreadingMixIn, SocketServer.TCPServer):
+    pass
+
+
+# Kjøres når programmet starter
+if __name__ == "__main__":
+    # Definer host og port for serveren
+    HOST = 'localhost'
+    PORT = 9999
+
+    # Sett opp serveren
+    server = ThreadedTCPServer((HOST, PORT), CLientHandler)
+
+    # Aktiver serveren. Den vil kjøre til den avsluttes med Ctrl+C
+    server.serve_forever()
+
