15d09e8fb4b61cb6a607c5d4af4ab2609253a763
Dag Inge Helgøy <dihelgoy@gmail.com>
2014-03-05 15:52:59 +0100

    Almost done yo

--- db2/db_raw.sql
+++ db2/db_raw.sql
@@ -1,19 +1,21 @@
-create schema kalendersystem
+create schema kalendersystem;
 
 create table user (
-    username varchar(255) not null,
-    password varchar(255) not null,
-    first_name varchar(255) not null,
-    sur_name varchar(255) not null,
-    mail varchar(255) not null,
+    username varchar(15) not null,
+    password varchar(45) not null,
+    first_name varchar(45) not null,
+    sur_name varchar(45) not null,
+    mail varchar(45) not null,
     phone char(12) not null,
-    primary key (username),
-    foreign key ()
+    primary key (username)
     );
 
 create table group (
-    groupname varchar(255) not null,
-    primary key (groupname)
+    groupname varchar(15) not null,
+    parent_groupname varchar(15),
+    primary key (groupname),
+    constraint subgroup foreign key parent_groupname references group(groupname)
+    	on update cascade
     );
 
 create table appointment (
@@ -21,26 +23,49 @@ create table appointment (
     starttime datetime not null,
     endtime datetime not null,
     description varchar(255),
-    location varchar(255),
-    primary key (id)
+    location varchar(45),
+    responsible_username varchar(15) not null,
+    primary key (id),
+    constraint responsible foreign key (responsible_username) references user(username)
+    	on delete cascade
+    	on update cascade
     );
 
 create table meetingroom (
-    roomnumber varchar(255) not null,
-    address varchar(255) not null,
+    roomnumber varchar(45) not null,
     capacity int not null,
-    primary key (roomnumber, address)
+    primary key (roomnumber)
     );
 
 create table notification (
     appointmentID int 
+    type varchar(15)
+    description varchar(255)
+	);
 
-create table participant {
-	username varchar(255) not null,
+create table participant (
+	username varchar(45) not null,
 	id int not null,
-	status varchar(255),
+	status varchar(45),
 	hidden boolean,
 	alarm int,
 	primary key(username, id),
-	}
+	constraint user_participant foreign key username references user(username)
+		on delete cascade
+		on update cascade,
+	constraint appointment_participant foreign key id references appointment(id)
+		on delete cascade
+		on update cascade
+	);
 
+create table member (
+	groupname varchar(15) not null,
+	username varchar(15) not null,
+	primary key (groupname, username),
+	constraint member_username foreign key username references user(username)
+		on delete cascade
+		on update cascade,
+	constraint member_of_group foreign key groupname references group(groupname)
+		on delete cascade
+		on update cascade
+	);
