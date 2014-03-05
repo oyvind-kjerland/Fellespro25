019e319d6b65b508c3687060ca879c492cbc4f23
Iver Egge <iver@iegget.com>
2014-03-05 11:59:20 +0100

    inital db

---
+++ db2/db_raw.sql
@@ -0,0 +1,11 @@
+create table user (
+    username varchar(255) not null,
+    password varchar(255) not null,
+    first_name varchar(255) not null,
+    last_name varchar(255) not null,
+    mail varchar(255) not null,
+    phone char(12) not null,
+    primary key (username)
+    );
+
+create table 
