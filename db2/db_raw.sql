18cda851b95388294dd536e677975598721035b7
Iver Egge <iver@iegget.com>
2014-03-05 12:41:31 +0100

    more db

--- db2/db_raw.sql
+++ db2/db_raw.sql
@@ -2,10 +2,35 @@ create table user (
     username varchar(255) not null,
     password varchar(255) not null,
     first_name varchar(255) not null,
-    last_name varchar(255) not null,
+    sur_name varchar(255) not null,
     mail varchar(255) not null,
     phone char(12) not null,
-    primary key (username)
+    primary key (username),
+    foreign key (
     );
 
-create table 
+create table group (
+    groupname varchar(255) not null,
+    primary key (groupname)
+    );
+
+create table appointment (
+    id int not null auto_increment,
+    starttime datetime not null,
+    endtime datetime not null,
+    description varchar(255),
+    location varchar(255),
+    primary key (id)
+    );
+
+create table meetingroom (
+    roomnumber varchar(255) not null,
+    address varchar(255) not null,
+    capacity int not null,
+    primary key (roomnumber, address)
+    );
+
+create table notification (
+    appointmentID int 
+
+
